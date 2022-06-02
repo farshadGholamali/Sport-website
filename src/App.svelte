10e1ecfd1e8a19fa38941eb0e1f3d7da936b1f06
Farshad gholamali <98264671+farshadGholamali@users.noreply.github.com>
2022-06-02 21:23:00 +0430

    build : add Sass to project

--- src/App.svelte
+++ src/App.svelte
@@ -20,7 +20,7 @@
   </p>
 </main>
 
-<style>
+<style lang=scss>
   :root {
     font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
       Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
