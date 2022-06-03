2c4ad5a6f1374412fd7205952a01dc3d282c0424
Farshad gholamali <98264671+farshadGholamali@users.noreply.github.com>
2022-06-03 18:00:09 +0430

    feat(Dropdown) : add DropdownItem component
    add DropdownItem component in src/@core/components/... path;

---
+++ src/@core/components/DropdownItem.svelte
@@ -0,0 +1,21 @@
+<script>
+  export let DropdownItems;
+</script>
+
+<ul
+  tabindex="0"
+  class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
+>
+  {#each DropdownItems as DropdownItem}
+    <li>
+      <a href={DropdownItem.hypertext} class={DropdownItem.classes}>
+        {DropdownItem.title}
+        {#if DropdownItem.notification}
+          <span class="badge">{DropdownItem.notification}</span>
+        {/if}
+      </a>
+    </li>
+  {/each}
+</ul>
+
+<style></style>
