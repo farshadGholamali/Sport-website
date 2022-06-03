96014cd5f29cdd84e07488263f072fb302b53601
Farshad gholamali <98264671+farshadGholamali@users.noreply.github.com>
2022-06-03 16:19:06 +0430

    refactor(Header) : add Button component to Header
    convert (Make a deposite) and (search) button tag to Button component;

--- src/components/combined-layout/Header.svelte
+++ src/components/combined-layout/Header.svelte
@@ -1,11 +1,28 @@
-<script>
+<script lang="ts">
+  // Components
+  import Button from "../../@core/components/Button.svelte";
+  // Interfaces
+  import type IButton from "../../@core/interface/IButton";
+  // Props
+  let BtnDeposit: IButton = {
+    tagType: "a",
+    hypertext: "/",
+    classes:
+      "btn min-h-min h-10 bg-red-500 hover:bg-red-400 border-transparent hover:border-transparent rounded-[20px] shadow-lg shadow-red-500/50",
+    text: "Make a deposit",
+  };
+  let BtnSearch: IButton = {
+    tagType: "button",
+    classes:
+      "btn min-h-min h-10 px-2.5 hover:border-transparent border-blue-400 bg-blue-100 hover:bg-blue-200",
+  };
 </script>
 
 <div class="navbar justify-between bg-base-100 ">
   <div class="flex-1 navbar-start">
     <a href="/" class="btn btn-ghost min-h-min h-10 normal-case text-xl"
-      >Football</a
-    >
+      >Football
+    </a>
   </div>
 
   <div class="tabs navbar-center">
@@ -18,14 +35,13 @@
   </div>
 
   <div class="flex navbar-end gap-4">
-    <a
-      href="/"
-      class="btn min-h-min h-10 bg-red-500 hover:bg-red-400 border-transparent hover:border-transparent rounded-[20px] shadow-lg shadow-red-500/50"
-      >make a deposite
-    </a>
-    <button
-      class="btn min-h-min h-10 px-2.5 hover:border-transparent border-blue-400 bg-blue-100 hover:bg-blue-200"
-    >
+    <Button
+      hypertext={BtnDeposit.hypertext}
+      tagType={BtnDeposit.tagType}
+      classes={BtnDeposit.classes}
+      text={BtnDeposit.text}
+    />
+    <Button tagType={BtnSearch.tagType} classes={BtnSearch.classes}>
       <svg
         xmlns="http://www.w3.org/2000/svg"
         class="h-5 w-5 stroke-cyan-800"
@@ -39,7 +55,8 @@
           d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
         />
       </svg>
-    </button>
+    </Button>
+
     <div class="dropdown dropdown-end">
       <label for="" tabindex="0" class="btn btn-ghost btn-circle avatar">
         <div class="w-10 rounded-full">
@@ -49,6 +66,7 @@
           />
         </div>
       </label>
+
       <ul
         tabindex="0"
         class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
@@ -67,6 +85,7 @@
         </li>
       </ul>
     </div>
+    
   </div>
 </div>
 
