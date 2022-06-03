a75fb27d4ffa74666ab1dbadcc02c39a8f6c0e14
Farshad gholamali <98264671+farshadGholamali@users.noreply.github.com>
2022-06-03 15:51:49 +0430

    feat(Button) : add Button component
    add Button component in src/@core/components/... path;

---
+++ src/@core/components/Button.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  export let classes: string = "";
+  export let text: string = "";
+  export let tagType: string = "";
+  export let hypertext: string = "";
+</script>
+
+{#if tagType === "button"}
+  <button class={classes}>{text}</button>
+{:else}
+  <a href={hypertext} class={classes}>{text}</a>
+{/if}
+
+<style>
+
+</style>
