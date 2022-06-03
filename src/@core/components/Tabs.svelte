49fe0dc5da7a0767f19eaa00bf7f56834b98c46a
Farshad gholamali <98264671+farshadGholamali@users.noreply.github.com>
2022-06-03 19:17:37 +0430

    feat(Tabs) :add Tabs component
    add Tabs component in src/@core/components/... path

---
+++ src/@core/components/Tabs.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  // Interface
+  import type ITabsItems from "../interface/ITabsItems";
+  // Props
+  export let TabsItems: ITabsItems[];
+</script>
+
+<div class="tabs navbar-center">
+  {#each TabsItems as TabItem}
+    <a href={TabItem.hypertext} class={TabItem.classes}>{TabItem.title}</a>
+  {/each}
+</div>
+
+<style></style>
