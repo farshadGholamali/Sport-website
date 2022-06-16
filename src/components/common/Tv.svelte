2649aa2672093561bae4b97a8ae35c710d1fbf33
Farshad gholamali <98264671+farshadGholamali@users.noreply.github.com>
2022-06-17 00:53:14 +0430

    feat(Tv): add Tv component
    add tv.svelte and show a test iframe;

---
+++ src/components/common/Tv.svelte
@@ -0,0 +1,28 @@
+<script>
+</script>
+
+<div class="w-full h-[36rem]">
+  <div class="h_iframe-aparat_embed_frame">
+    <span style="display: block;padding-top: 57%" />
+    <iframe
+      src="https://www.aparat.com/video/video/embed/videohash/3W48G/vt/frame?recom=none"
+      title="livestream"
+    />
+  </div>
+</div>
+
+<style>
+  .h_iframe-aparat_embed_frame {
+    position: relative;
+    height: 100%;
+  }
+
+  .h_iframe-aparat_embed_frame iframe {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    border-radius: 1rem;
+  }
+</style>
