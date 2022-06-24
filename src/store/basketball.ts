4209fbfd1b4b8c1052ca64cba1e2809763bf3d2d
Farshad gholamali <98264671+farshadGholamali@users.noreply.github.com>
2022-06-24 21:27:50 +0430

    feat(basketball): add getGames api

--- src/store/basketball.ts
+++ src/store/basketball.ts
@@ -136,13 +136,13 @@ export const Basketball_getTeamsStatistics = readable(async (league: number, sea
 // Get the standings for a league.
 // Return a table of one or more rankings according to the league / cup. Some competitions have several rankings in a year, regular season, pre season etc…
 // To know the list of available stages or groups you have to use the endpoint standings/stages or standings/groups
-export const Basketball_getStandings = readable(async (league: number, season: number, team: number, stage?: string, group?: string) => {
+export const Basketball_getStandings = readable(async (league: number, season: string, team?: number, stage?: string, group?: string) => {
     try {
         const response = await axios.get("https://v1.Basketball.api-sports.io/standings", {
             params: {
                 league,
                 season,
-                team,
+                ...(team && { team: team }),
                 ...(stage && { stage: stage }),
                 ...(group && { group: group }),
             },
@@ -179,10 +179,34 @@ export const Basketball_getStandingsStage = readable(async (league: number, seas
 // Get the list of available groups for a league to be used in the standings endpoint.
 export const Basketball_getStandingsgroups = readable(async (league: number, season: number) => {
     try {
-        const response = await axios.get("https://v1.Basketball.api-sports.io/standings/groups", {
+        const response = await axios.get("https://v1.basketball.api-sports.io/standings/groups", {
             params: {
                 league,
                 season,
+            },
+            headers: {
+                "X-RapidAPI-Key": "3ecc786cf98a929cf086263756f6bb88"
+            }
+        })
+        return response.data.response
+    } catch (err) {
+        console.log(err);
+    }
+})
+
+// Games
+// For all requests to games you can add the query parameter timezone to your request in order to retrieve the list of games in the time zone of your choice like “Europe/London“
+// To know the list of available time zones you have to use the endpoint timezone
+export const Basketball_getGames = readable(async (id?:number,date?:string,league?: number, season?: string,team?:number,timezone?:string) => {
+    try {
+        const response = await axios.get("https://v1.basketball.api-sports.io/games", {
+            params: {
+                ...(id && { id: id }),
+                ...(date && { date: date }),
+                ...(league && { league: league }),
+                ...(season && { season: season }),
+                ...(team && { team: team }),
+                ...(timezone && { timezone: timezone }),
             },
             headers: {
                 "X-RapidAPI-Key": "3ecc786cf98a929cf086263756f6bb88"
