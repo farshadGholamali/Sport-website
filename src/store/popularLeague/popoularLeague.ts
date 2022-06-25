81dcb6ca1df0d80a54c28964b3536f3736879f85
Farshad gholamali <98264671+farshadGholamali@users.noreply.github.com>
2022-06-25 23:02:10 +0430

    feat: add popularLeague file

---
+++ src/store/popularLeague/popoularLeague.ts
@@ -0,0 +1,97 @@
+export let footballLeague = [
+    {
+        name: "Premier League",
+        id: "39",
+    },
+    {
+        name: "Serie_A",
+        id: "135",
+    },
+    {
+        name: "LaLiga",
+        id: "140",
+    },
+    {
+        name: "Bundesliga",
+        id: "78",
+    },
+    {
+        name: "Ligue 1",
+        id: "61",
+    },
+    {
+        name: "UEFA Champions League",
+        id: "2",
+    },
+    {
+        name: "UEFA Europa League",
+        id: "3",
+    },
+    {
+        name: "Euro Championship",
+        id: "4",
+    },
+    {
+        name: "UEFA Nations League",
+        id: "5",
+    },
+    {
+        name: "Africa Cup of Nations",
+        id: "6",
+    },
+    {
+        name: "Asian Cup",
+        id: "7",
+    },
+    {
+        name: "Copa America",
+        id: "9",
+    },
+    {
+        name: "Friendlies",
+        id: "10",
+    },
+    {
+        name: "AFC Champions League",
+        id: "17",
+    },
+    {
+        name: "Iran : Persian Gulf Cup",
+        id: "290",
+    },
+];
+
+export let basketballLeague = [
+    {
+        name: "NBA",
+        id: "12",
+    },
+    {
+        name: "Liga Femenina W",
+        id: "94",
+    },
+    {
+        name: "Lega A",
+        id: "52",
+    },
+    {
+        name: "LNB",
+        id: "2",
+    },
+    {
+        name: "NBL",
+        id: "1",
+    },
+    {
+        name: "Liga A",
+        id: "18",
+    },
+    {
+        name: "Olympic Games",
+        id: "192",
+    },
+    {
+        name: "Friendly International",
+        id: "205",
+    },
+];
